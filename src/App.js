0dd493cbfa5a79c5a58ab947aae9408ff900c051
piyushloona <112817518+piyushloona@users.noreply.github.com>
2023-05-17 17:38:12 +0530

    new code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import './Assets/sass/style.scss'
 import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
-import Dashboard_page from './Assets/Components/Dashboard/Dashboard';
+import Dashboard_page from '../src/Components/Dashboard/Dashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Sidebar from '../src/Assets/Components/Dashboard/Sidebar'
-import Header from '../src/Assets/Components/Dashboard/Header'
-import Home from '../src/Assets/Components/Dashboard/home'
-import Table from './Assets/Components/Dashboard/pages_components/Table';
-import Form from './Assets/Components/Dashboard/pages_components/Form';
+import Sidebar from '../src/Components/Dashboard/Sidebar'
+import Header from '../src/Components/Dashboard/Header'
+import Home from '../src/Components/Dashboard/home'
+import Table from './Components/Dashboard/pages_components/Table';
+import Form from './Components/Dashboard/pages_components/Form';
+import Footer from './Components/Dashboard/pages_components/Footer';
 function App() {
   function Dashboard(){
       return(
@@ -17,6 +18,8 @@ function App() {
           <Header/>
           <Sidebar/>
           <Outlet/>
+          <Footer/>
+          
       </>
       
       )
